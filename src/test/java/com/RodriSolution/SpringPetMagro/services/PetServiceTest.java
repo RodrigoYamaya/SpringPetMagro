3c01abd70dc68574e1f6417da97f28b0a316f31d
rodrigo <kakakpegado@hotmail.com>
2025-07-15 23:16:27 -0300

    test: adiciona testes para delete/findById e ajusta listarTodosPets

--- src/test/java/com/RodriSolution/SpringPetMagro/services/PetServiceTest.java
+++ src/test/java/com/RodriSolution/SpringPetMagro/services/PetServiceTest.java
@@ -1,6 +1,5 @@
 package com.RodriSolution.SpringPetMagro.services;
 
-import com.RodriSolution.SpringPetMagro.domain.PetValidator;
 import com.RodriSolution.SpringPetMagro.exceptions.*;
 import com.RodriSolution.SpringPetMagro.model.dtos.PetResquestDto;
 import com.RodriSolution.SpringPetMagro.model.entities.Endereco;
@@ -11,12 +10,12 @@ import com.RodriSolution.SpringPetMagro.model.enums.Tipo;
 import com.RodriSolution.SpringPetMagro.repositories.PetRepository;
 import com.RodriSolution.SpringPetMagro.repositories.TutorRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
@@ -24,9 +23,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -43,6 +42,14 @@ class PetServiceTest {
     @Mock
     private Tutor tutor;
 
+
+    @BeforeEach
+    public void setUp() {
+        Pet pet = new Pet(1L, Tipo.GATO, "@@@@", "@@@", Sexo.FEMEA, new Endereco("rio", "53", "5"), 4.0, 4.0, "indefida ", LocalDate.parse("2010-05-30"), tutor);
+        lenient().when(petRepository.findById(1L)).thenReturn(Optional.of(pet));
+        lenient().when(petRepository.findAll()).thenReturn(Collections.singletonList(pet));
+
+    }
     @Test
     @DisplayName("Deve lançar exceção se nome ou sobrenome do pet forem inválidos")
     void deveLancarExcecaoSeNomeOuSobrenomeInvalido() {
@@ -54,7 +61,7 @@ class PetServiceTest {
 
         PetResquestDto petResquestDto1 = new PetResquestDto(Tipo.GATO, "@@@@", "@@@", Sexo.FEMEA, new Endereco("rio", "53", "5"), 4.0, 4.0, "indefida ", 1L, LocalDate.parse("2010-05-30"));
 
-        Assertions.assertThrows(NomeInvalidoException.class, () -> petService.savePet(petResquestDto1));
+        assertThrows(NomeInvalidoException.class, () -> petService.savePet(petResquestDto1));
 
     }
 
@@ -69,7 +76,7 @@ class PetServiceTest {
         PetResquestDto petDto = new PetResquestDto(Tipo.CACHORRO, "Rex", "Dogão", Sexo.MACHO, new Endereco("Rua A", "10", "2"), 70.0, 5.0, "Pitbull", // peso inválido (acima de 60)
                 1L, LocalDate.parse("2018-01-01"));
 
-        Assertions.assertThrows(PesoInvalidaException.class, () -> petService.savePet(petDto));
+        assertThrows(PesoInvalidaException.class, () -> petService.savePet(petDto));
     }
 
     @Test
@@ -85,11 +92,11 @@ class PetServiceTest {
                 "Brabo",
                 Sexo.MACHO,
                 new Endereco("Rua C", "45", "4"),
-                10.0, 4.0, "P!tbull", // raça inválida
+                10.0, 4.0, "P!tbull",
                 1L, LocalDate.parse("2016-09-10")
         );
 
-        Assertions.assertThrows(RacaInvalidaException.class, () ->
+        assertThrows(RacaInvalidaException.class, () ->
                 petService.savePet(petDto)
         );
     }
@@ -107,12 +114,12 @@ class PetServiceTest {
                 "Bolt",
                 "Correria",
                 Sexo.MACHO,
-                new Endereco("Av. Principal", "10A", "Ap"), // número inválido (tem letra)
+                new Endereco("Av. Principal", "10A", "Ap"),
                 8.0, 3.0, "Golden",
                 1L, LocalDate.parse("2020-02-02")
         );
 
-        Assertions.assertThrows(EnderecoInvalidoException.class, () ->
+        assertThrows(EnderecoInvalidoException.class, () ->
                 petService.savePet(petDto)
         );
     }
@@ -120,14 +127,13 @@ class PetServiceTest {
 
     @Test
     void retornarTodoPets() {
-        Pet pet = new Pet();
-        when(petRepository.findAll()).thenReturn(Collections.singletonList(pet));
-
         List<Pet> pets = petService.listarPets();
 
-        assertNotNull(pets);
-        assertEquals(1, pets.size());
-        assertEquals(pet, pets.get(0));
+        assertEquals(0, pets.size());
+
+        verify(petRepository).findAll();
+        verifyNoMoreInteractions(petRepository);
+
     }
 
     @Test
@@ -137,15 +143,25 @@ class PetServiceTest {
 
     @Test
     void findById() {
+        Pet petId = petService.findById(1L);
+        assertNotNull(petId);
     }
 
     @Test
-    void deletarPet() {
+    @DisplayName("Deve lançar exceção ao tentar deletar pet inexistente")
+    void deveLancarExcecaoAoDeletarPetInexistente() {
+        Long petIdInexistente = 999L;
+        assertThrows(RecursoNaoEncontrado.class, () ->
+                petService.deletarPet(petIdInexistente));
     }
+
 
     @Test
     void updatePet() {
+
     }
+
 }
+
 
 
